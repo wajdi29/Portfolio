a86caa73fc44221bdae4af99af60cbc032512b38
Wajdi <wajdidouiri26@gmail.com>
2020-11-11 08:54:23 +0100

    done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+$(window).on('load', function () {
+    $('.loader .inner').fadeOut(700, function () {
+        $(".loader").fadeOut(900)
+    })
+    $(".items").isotope({
+        filter: "*",
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+})
+
+
+
 $(document).ready(function () {
 
     $('#slides').superslides({
@@ -72,14 +88,46 @@ $(document).ready(function () {
     });
 
     $("[data-fancybox]").fancybox()
-    $(".items").isotope({
-        filter: "*",
-        animationOptions: {
-            duration: 1500,
-            easing: 'linear',
-            queue: false
-        }
+
+    $('#filters a').click(function () {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        const selector = $(this).attr("data-filter")
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false
     })
+    $('#navigation li a').click(function (e) {
+        e.preventDefault()
+        const targetElement = $(this).attr('href')
+        const targetPosition = $(targetElement).offset().top
+        $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow")
+
+    })
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+    const stickyNavigation = () => {
+        const body = $("body")
+        if ($(window).scrollTop() >= navTop) {
+            body.css('padding-top', nav.outerHeight() + "px")
+            body.addClass('fixedNav')
+        } else {
+            body.css('padding-top', 0)
+            body.removeClass('fixedNav')
+        }
+
+
+    }
+    $(window).on("scroll", stickyNavigation)
 
 
 });
